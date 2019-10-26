9db8a5ade843771d05e0ee601763ea3a2a1c68c3
NikitaPO <gigzim345@gmai.com>
2019-10-27 00:00:49 +0300

    objects this

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
-"use strict";
+'use strict';
+console.clear();
 
 // module.exports.pow = pow;
 // module.exports.min = min;
@@ -541,3 +542,94 @@ function multiplyNumeric(obj){
 }
 
 //4.4
+/**
+ * Создайте объект calculator (калькулятор) с тремя методами:
+
+read() (читать) запрашивает два значения и сохраняет их как свойства объекта.
+sum() (суммировать) возвращает сумму сохранённых значений.
+mul() (умножить) перемножает сохранённые значения и возвращает результат.
+let calculator = {
+  // ... ваш код ...
+};
+
+calculator.read();
+alert( calculator.sum() );
+alert( calculator.mul() );
+ */
+
+ let id = Symbol('id');
+
+ let calculator = {
+
+   [id]: 0,
+
+   read(...value) {
+     for (let item in value) {
+       this[this[id]] = item;
+       this[id]++;
+     }
+   },
+
+   sum() {
+     let sum = 0;
+
+     for(let key in this) {
+       sum += this[key];
+     }
+   },
+
+   mul() {
+     let multiply = 1;
+
+     for(let key in this) {
+       if (typeof this[key] === 'number') {
+         multiply *= this[key];
+       }
+     }
+
+     return multiply;
+   }
+ };
+
+/**
+ * Это ladder (лестница) – объект, который позволяет подниматься вверх и спускаться:
+
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+  },
+  down() {
+    this.step--;
+  },
+  showStep: function() { // показывает текущую ступеньку
+    alert( this.step );
+  }
+};
+Теперь, если нам нужно сделать несколько последовательных вызовов, мы можем выполнить это так:
+
+ladder.up();
+ladder.up();
+ladder.down();
+ladder.showStep(); // 1
+Измените код методов up, down и showStep таким образом, чтобы их вызов можно было сделать по цепочке, например так:
+
+ladder.up().up().down().showStep(); // 1
+Такой подход широко используется в библиотеках JavaScript.
+ */
+
+ let ladder = {
+   step: 0,
+   up() {
+     this.step++;
+     return this;
+   },
+   down() {
+     this.step--;
+     return this;
+   },
+   showStep: function() { // показывает текущую ступеньку
+     console.log( this.step );
+     return this;
+   }
+ };
