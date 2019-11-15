bfa34335cfaa6a43ae59bb1383e84096e8b3f625
NikitaPO <gigzim345@gmail.com>
2019-11-16 00:54:14 +0300

    arrays almost

--- app/index.js
+++ app/index.js
@@ -1062,3 +1062,274 @@ function camelize(str) {
       index == 0 ? word : word[0].toUpperCase() + word.slice(1) )
     .join('');
 }
+
+/*
+Напишите функцию filterRange(arr, a, b), которая принимает массив arr, ищет в нём элементы между a и b и отдаёт массив этих элементов.
+
+Функция должна возвращать новый массив и не изменять исходный.
+
+Например:
+
+let arr = [5, 3, 8, 1];
+
+let filtered = filterRange(arr, 1, 4);
+
+alert( filtered ); // 3,1 (совпадающие значения)
+
+alert( arr ); // 5,3,8,1 (без изменений)
+*/
+
+function filterRange(arr, a, b) {
+  return arr.filter( item => (item >= a && item <= b));
+}
+
+/*
+
+Напишите функцию filterRangeInPlace(arr, a, b), которая принимает массив arr и удаляет из него все значения кроме тех, которые находятся между a и b. То есть, проверка имеет вид a ≤ arr[i] ≤ b.
+
+Функция должна изменять принимаемый массив и ничего не возвращать.
+
+Например:
+
+let arr = [5, 3, 8, 1];
+
+filterRangeInPlace(arr, 1, 4); // удалены числа вне диапазона 1..4
+
+alert( arr ); // [3, 1]
+*/
+
+function filterRangeInPlace(arr, a, b) {
+  for ( let i = 0; i < arr.length; i++ ) {
+    if ( arr[i] < a || arr[i] > b ) {
+      arr.splice(i--, 1);
+    }
+  }
+}
+
+/*
+Сортировать в порядке по убыванию
+важность: 4
+let arr = [5, 2, 1, -10, 8];
+
+// ... ваш код для сортировки по убыванию
+
+alert( arr ); // 8, 5, 2, 1, -10
+*/
+//
+// let array = [1,3,2,2,1,4,3,4];
+//
+// array.sort((a, b) => b - a);
+//
+// console.log(array);
+
+
+/*
+У нас есть массив строк arr. Нужно получить отсортированную копию, но оставить arr неизменённым.
+
+Создайте функцию copySorted(arr), которая будет возвращать такую копию.
+
+let arr = ["HTML", "JavaScript", "CSS"];
+
+let sorted = copySorted(arr);
+
+alert( sorted ); // CSS, HTML, JavaScript
+alert( arr ); // HTML, JavaScript, CSS (без изменений)
+*/
+
+function copySorted(arr) {
+  return arr.slice().sort();
+}
+
+// let arr = ["HTML", "JavaScript", "CSS"];
+//
+// let sorted = copySorted(arr);
+//
+// console.log(arr, sorted);
+
+/*
+Создайте функцию конструктор Calculator, которая создаёт «расширяемые» объекты калькулятора.
+
+Задание состоит из двух частей.
+
+Во-первых, реализуйте метод calculate(str), который принимает строку типа "1 + 2" в формате «ЧИСЛО оператор ЧИСЛО» (разделено пробелами) и возвращает результат. Метод должен понимать плюс + и минус -.
+
+Пример использования:
+
+let calc = new Calculator;
+
+alert( calc.calculate("3 + 7") ); // 10
+Затем добавьте метод addMethod(name, func), который добавляет в калькулятор новые операции. Он принимает оператор name и функцию с двумя аргументами func(a,b), которая описывает его.
+
+Например, давайте добавим умножение *, деление / и возведение в степень **:
+
+let powerCalc = new Calculator;
+powerCalc.addMethod("*", (a, b) => a * b);
+powerCalc.addMethod("/", (a, b) => a / b);
+powerCalc.addMethod("**", (a, b) => a ** b);
+
+let result = powerCalc.calculate("2 ** 3");
+alert( result ); // 8
+Для этой задачи не нужны скобки или сложные выражения.
+Числа и оператор разделены ровно одним пробелом.
+Не лишним будет добавить обработку ошибок.
+*/
+
+function Calculator() {
+  this.methods = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+  };
+
+
+  this.calculate = (str) => {
+    let split = str.split(' ');
+    let a = +split[0];
+    let operator = split[1];
+    let b = +split[2];
+    let result = this.methods[operator](a, b);
+
+    if( isNaN(a) || isNaN(b) || !result ) {
+      return NaN;
+    }
+
+    return result;
+  };
+
+  this.addMethod = (operator, fn) => {
+    this.methods[operator] = fn;
+  };
+}
+
+let myCalc = new Calculator();
+
+myCalc.addMethod('**', (a, b) => a ** b);
+myCalc.addMethod('/', (a, b) => a / b);
+myCalc.addMethod('*', (a, b) => a * b);
+
+// console.log(myCalc.calculate('6 * 2'));
+
+
+/*
+У вас есть массив объектов user, и в каждом из них есть user.name. Напишите код, который преобразует их в массив имён.
+
+Например:
+*/
+//
+// let vasya = { name: "Вася", age: 25 };
+// let petya = { name: "Петя", age: 30 };
+// let masha = { name: "Маша", age: 28 };
+//
+// let users = [ vasya, petya, masha ];
+//
+// let names = users.map((item) => item.name)
+//
+// console.log( names ); // Вася, Петя, Маша
+
+/*
+У вас есть массив объектов user, и у каждого из объектов есть name, surname и id.
+
+Напишите код, который создаст ещё один массив объектов с параметрами id и fullName, где fullName – состоит из name и surname.
+
+Например:
+*/
+//
+// let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+// let petya = { name: "Петя", surname: "Иванов", id: 2 };
+// let masha = { name: "Маша", surname: "Петрова", id: 3 };
+//
+// let users = [ vasya, petya, masha ];
+//
+// let usersMapped = users.map(item => {
+//   return {
+//     fullName : item.name + ' ' + item.surname,
+//     id : item.id
+//   }
+// });
+
+/*
+usersMapped = [
+  { fullName: "Вася Пупкин", id: 1 },
+  { fullName: "Петя Иванов", id: 2 },
+  { fullName: "Маша Петрова", id: 3 }
+]
+*/
+//
+// console.log( usersMapped[0].id ) // 1
+// console.log( usersMapped[0].fullName ) // Вася Пупкин
+
+/*
+Напишите функцию sortByAge(users), которая принимает массив объектов со свойством age и сортирует их по нему.
+
+Например:
+*/
+// let vasya = { name: "Вася", age: 25 };
+// let petya = { name: "Петя", age: 30 };
+// let masha = { name: "Маша", age: 28 };
+//
+// let arr = [ vasya, petya, masha ]
+//
+// sortByAge(arr);
+//
+// // теперь: [vasya, masha, petya]
+// console.log(arr[0].name); // Вася
+// console.log(arr[1].name); // Маша
+// console.log(arr[2].name); // Петя
+//
+// console.log(arr)
+//
+// function sortByAge(arr) {
+//   arr.sort((a, b) => (a.age - b.age))
+// }
+
+/*
+Напишите функцию shuffle(array), которая перемешивает (переупорядочивает случайным образом) элементы массива.
+
+Многократные прогоны через shuffle могут привести к разным последовательностям элементов. Например:
+
+let arr = [1, 2, 3];
+
+shuffle(arr);
+// arr = [3, 2, 1]
+
+shuffle(arr);
+// arr = [2, 1, 3]
+
+shuffle(arr);
+// arr = [3, 1, 2]
+// ...
+Все последовательности элементов должны иметь одинаковую вероятность. Например, [1,2,3] может быть переупорядочено как [1,2,3] или [1,3,2], или [3,1,2] и т.д., с равной вероятностью каждого случая.
+
+*/
+
+function shuffle(arr) {
+  for ( let i = arr.length - 1; i > 0; i-- ) {
+    let r = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[r]] = [arr[r], arr[i]];
+  }
+}
+//
+// let count = {
+//   '123': 0,
+//   '132': 0,
+//   '213': 0,
+//   '231': 0,
+//   '312': 0,
+//   '321': 0,
+// }
+
+// let count = {
+//   '0': 0,
+//   '1': 0,
+//   '2': 0,
+//   '3': 0,
+//   '4': 0,
+//   '5': 0,
+//   '6': 0,
+// }
+//
+// for (let i = 0; i < 10000; i++) {
+//   let randomNumber = Math.ceil((Math.random() * 5) - 1);
+//   count[randomNumber]++;
+// }
+//
+// console.log(count);
