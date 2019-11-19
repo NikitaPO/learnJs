043c3ab80e0b40bde2e73d62114f8a56cfe98e5e
NikitaPO <gigzim345@gmail.com>
2019-11-19 11:53:22 +0300

    single-linked list

--- app/index.js
+++ app/index.js
@@ -1999,3 +1999,262 @@ let users = {
 //     return value
 //   }
 // }));
+
+function powRecursion(x, n) {
+  return (n == 1) ? x : x * pow(x, n - 1)
+}
+
+function powIteration(x, n) {
+  let result = x;
+
+  for (let i = 1; i < n; i++) {
+    result *= x;
+  }
+
+  return result;
+}
+
+function benchmark(fn, ...args) {
+  let result = 0;
+
+  for (let i = 0; i < 100000; i++) {
+    let start = Date.now();
+    fn(...args);
+    let end = Date.now();
+    result += end - start;
+  }
+
+  return result
+}
+
+function test(fn, ...args) {
+  let result = 0;
+
+  for (let i = 0; i <= 100; i++) {
+    result += benchmark(fn, ...args);
+  }
+
+  return `${result / 100}ms`
+}
+
+/*
+Напишите функцию sumTo(n), которая вычисляет сумму чисел 1 + 2 + ... + n.
+
+Например:
+
+sumTo(1) = 1
+sumTo(2) = 2 + 1 = 3
+sumTo(3) = 3 + 2 + 1 = 6
+sumTo(4) = 4 + 3 + 2 + 1 = 10
+...
+sumTo(100) = 100 + 99 + ... + 2 + 1 = 5050
+Сделайте три варианта решения:
+
+С использованием цикла.
+Через рекурсию, т.к. sumTo(n) = n + sumTo(n-1) for n > 1.
+С использованием формулы арифметической прогрессии.
+Пример работы вашей функции:
+
+function sumTo(n) { }
+
+alert( sumTo(100) ); // 5050
+P.S. Какой вариант решения самый быстрый? Самый медленный? Почему?
+
+P.P.S. Можно ли при помощи рекурсии посчитать sumTo(100000)?
+*/
+
+function sumToIter(n) {
+  let sum = n;
+
+  while (n > 1) {
+    sum += --n;
+  }
+
+  return sum
+}
+
+function sumToRec(n) {
+  if(n == 1) {
+    return 1
+  } else {
+    return n + sumToRec(n - 1)
+  }
+}
+
+function sumToRecTern(n) {
+  return (n == 1) ? 1 : n + sumToRecTern(n - 1)
+}
+
+function sumToFormula(n) {
+  return n * (n + 1) / 2
+}
+
+// let a = 5;
+//
+// console.log(test(sumToIter, a));
+// console.log(test(sumToRec, a));
+// console.log(test(sumToRecTern,a));
+// console.log(test(sumToFormula, a));
+
+/*
+Факториал натурального числа – это число, умноженное на "себя минус один", затем на "себя минус два", и так далее до 1. Факториал n обозначается как n!
+
+Определение факториала можно записать как:
+
+n! = n * (n - 1) * (n - 2) * ...*1
+Примеры значений для разных n:
+
+1! = 1
+2! = 2 * 1 = 2
+3! = 3 * 2 * 1 = 6
+4! = 4 * 3 * 2 * 1 = 24
+5! = 5 * 4 * 3 * 2 * 1 = 120
+Задача – написать функцию factorial(n), которая возвращает n!, используя рекурсию.
+
+alert( factorial(5) ); // 120
+P.S. Подсказка: n! можно записать как n * (n-1)! Например: 3! = 3*2! = 3*2*1! = 6
+*/
+
+function factorial(n) {
+  return n ? n * factorial(n - 1) : 1
+}
+
+/*
+Последовательность чисел Фибоначчи определяется формулой Fn = Fn-1 + Fn-2. То есть, следующее число получается как сумма двух предыдущих.
+
+Первые два числа равны 1, затем 2(1+1), затем 3(1+2), 5(2+3) и так далее: 1, 1, 2, 3, 5, 8, 13, 21....
+
+Числа Фибоначчи тесно связаны с золотым сечением и множеством природных явлений вокруг нас.
+
+Напишите функцию fib(n) которая возвращает n-е число Фибоначчи.
+
+Пример работы:
+
+function fib(n) { }
+
+alert(fib(3)); // 2
+alert(fib(7)); // 13
+alert(fib(77)); // 5527939700884757
+P.S. Все запуски функций из примера выше должны работать быстро. Вызов fib(77) должен занимать не более доли секунды.
+*/
+
+function fib(n) {
+  let sum;
+
+  for(let a = 1, b = 1, i = 2; i < n; i++) {
+    sum = a + b;
+    a = b;
+    b = sum;
+  }
+
+  return sum
+}
+
+// function fib(n) {
+//   return (n < 3) ? 1 : fib(n - 1) + fib(n - 2)
+// }
+
+// function fib(n) {
+//   //самый быстрый, но не точный метод
+//   const gSec = +((1 + Math.sqrt(5)) / 2).toFixed(15);
+//   console.log(gSec);
+//   return (+Math.pow(gSec, n).toFixed(15) - +Math.pow(-gSec, -n).toFixed(15)) / (2 * gSec - 1)
+// }
+
+// let fibArr = [];
+//
+// for(let i = 1; i <= 10; i++) {
+//   fibArr.push(fib(i));
+// }
+//
+// console.log(fibArr);
+
+// console.log(fib(5));
+// console.log(fib(77), test(fib, 77));
+
+/*
+Допустим, у нас есть односвязный список (как описано в главе Рекурсия и стек):
+
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null
+      }
+    }
+  }
+};
+Напишите функцию printList(list), которая выводит элементы списка по одному.
+
+Сделайте два варианта решения: используя цикл и через рекурсию.
+
+Как лучше: с рекурсией или без?
+*/
+
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null
+      }
+    }
+  }
+};
+
+function printList(list) {
+  console.log(list.value);
+
+  if (list.next) {
+    printList(list.next);
+  }
+}
+//
+// function printList(list) {
+//   let temp = list;
+//
+//   while (temp) {
+//     console.log(temp.value);
+//     temp = temp.next;
+//   }
+// }
+
+// printList(list);
+/*
+Выведите односвязный список из предыдущего задания Вывод односвязного списка в обратном порядке.
+
+Сделайте два решения: с использованием цикла и через рекурсию.
+*/
+//
+// function printListReverse(list) {
+//   if (list.next) {
+//     printListReverse(list.next);
+//   }
+//
+//   console.log(list.value);
+// }
+
+function printListReverse(list) {
+  let temp = list;
+  let arr = [];
+
+  while (temp) {
+    arr.push(temp.value);
+    temp = temp.next;
+  }
+
+  for (let i = arr.length - 1; i >= 0; i--){
+    console.log(arr[i]);
+  }
+}
+
+// printListReverse(list);
+
+// let array = new Array(1,2);
